b1c8bd0a21ea4b85a1f9e7dad06a2440d9c5bd24
Romain Penaruiz <rom.penaruiz@gmail.com>
2020-05-29 09:33:14 -0400

    layout for all the pages added with footer setup at the bottom

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,11 @@
 import React from "react"
+import Layout from "../components/layout"
 
 const Contact = () => {
   return (
-    <div>
+    <Layout>
       <h1>Hello from Contact</h1>
-    </div>
+    </Layout>
   )
 }
 
--- src/pages/tour.js
+++ src/pages/tour.js
@@ -1,10 +1,10 @@
 import React from "react"
-
+import Layout from "../components/layout"
 const Tour = () => {
   return (
-    <div>
+    <Layout>
       <h1>Hello from Tour</h1>
-    </div>
+    </Layout>
   )
 }
 
